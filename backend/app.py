9cce00f300f44866125e2d3b37f45a738d2b4f03
Seyed Parsa Neshaei <seyyedparsa@outlook.com>
2023-04-14 18:16:54 +0330

    Fixed bugs with server

--- backend/app.py
+++ backend/app.py
@@ -52,35 +52,46 @@ initialMessages = [
     "Feedback improves critical thinking skills and leads to better decision-making"
 ]
 
+initialMessages_evaluation = []
 
-def createGPTMessagesArray(currentIdeas):
-    messages = [{"role": "system", "content": "You provide one example idea per response. Give only the idea without any preamble or comment. Be as brief as possible."}, {"role": "user", "content": "Give me an example idea that convinces a bright but stubborn 20-year old male student that was asked by his professor whether he wants feedback on his latest term paper to agree to receive the feedback."}]
-    for idea in currentIdeas:
-        messages.append({"role": "assistant", "content": idea})
-        messages.append({"role": "user", "content": "Do the same but with a new idea."})
+
+def createGPTMessagesArray(currentIdeas, currentPage, currentText):
+    cleanedText = currentText.replace('"', "'")
+    messages = []
+    if currentPage == "study":
+        messages = [{"role": "system", "content": "You provide one example idea per response. Give only the idea without any preamble or comment. Be as brief as possible."}, {"role": "user", "content": "Give me an example idea that convinces a bright but stubborn 20-year old male student that was asked by his professor whether he wants feedback on his latest term paper to agree to receive the feedback."}]
+        for idea in currentIdeas:
+            messages.append({"role": "assistant", "content": idea})
+            messages.append({"role": "user", "content": "Do the same but with a new idea."})
+    elif currentPage == "evaluation":
+        messages = [{"role": "system", "content": "You evaluate text and give one suggestion for improvement. Give only the suggestion without any preamble or comment. Be as brief as possible."}, {"role": "user", "content": "This is my message: \"" + cleanedText + "\""}]
+        for idea in currentIdeas:
+            messages.append({"role": "assistant", "content": idea})
+            messages.append({"role": "user", "content": "Do the same but with a completely new suggestion."})
     return messages
 
 def getNewIdeaFromGPT(messages):
-    print("getting from GPT:" + str(messages))
+    print("Getting from GPT:" + str(messages))
     msg = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=messages
     )
     new_idea = msg['choices'][0]['message']['content']
-    print("got from GPT:" + str(new_idea))
+    print("Got from GPT:" + str(new_idea))
     return new_idea
 
 @app.route('/getNewExampleIdea', methods=['POST'])
 def get_new_example_idea():
     username = request.json['username']
     text = request.json['text']
+    currentPage = request.json['currentPage']
     print("got request from '" + username + "' with text: " + text)
     lock.acquire()
     try:
         ideas_data = load_data_from_file("ideas")
         for user in ideas_data:
             if user.username == username:
-                messages = createGPTMessagesArray(user.ideas)
+                messages = createGPTMessagesArray(user.ideas, currentPage, text)
                 new_idea = getNewIdeaFromGPT(messages)
                 user.ideas.append(new_idea)
                 save_data_to_file("ideas", ideas_data)
@@ -89,12 +100,13 @@ def get_new_example_idea():
                 save_data_to_file("each_idea", each_idea_data)
                 lock.release()
                 return jsonify({"success": True, "idea": new_idea})
-        messages = createGPTMessagesArray(initialMessages)
+        initials = initialMessages if currentPage == "study" else initialMessages_evaluation
+        messages = createGPTMessagesArray(initials, currentPage, text)
         new_idea = getNewIdeaFromGPT(messages)
-        ideas_data.append({"username": username, "ideas": initialMessages + [new_idea]})
+        ideas_data.append({"username": username, "ideas": initials + [new_idea]})
         save_data_to_file("ideas", ideas_data)
         each_idea_data = load_data_from_file("each_idea")
-        each_idea_data.append({"username": username, "text": text, "idea": new_idea, "time": int(time.time())})
+        each_idea_data.append({"username": username, "text": text, "idea": new_idea, "currentPage": currentPage, "time": int(time.time())})
         save_data_to_file("each_idea", each_idea_data)
         lock.release()
         return jsonify({"success": True, "idea": new_idea})
@@ -108,13 +120,15 @@ def get_new_example_idea():
 def submit():
     text = request.json['text']
     username = request.json['username']
-    group = request.json['group']
+    studyGroup = request.json['studyGroup']
+    evaluationGroup = request.json['evaluationGroup']
+    currentPage = request.json['currentPage']
     keystrokes = request.json['keystrokes']
     helpTaps = request.json['helpTaps']
     lock.acquire()
     try:
         submissions_data = load_data_from_file("submissions")
-        submissions_data.append({"username": username, "text": text, "group": group, "time": int(time.time())})
+        submissions_data.append({"username": username, "text": text, "studyGroup": studyGroup, "evaluationGroup": evaluationGroup, "currentPage": currentPage, "time": int(time.time())})
         save_data_to_file("submissions", submissions_data)
     except Exception as e:
         print(e)
@@ -124,7 +138,7 @@ def submit():
 
     try:
         keystrokes_data = load_data_from_file("keystrokes")
-        keystrokes_data.append({"username": username, "keystrokes": keystrokes, "time": int(time.time())})
+        keystrokes_data.append({"username": username, "keystrokes": keystrokes, "time": int(time.time()), "currentPage": currentPage})
         save_data_to_file("keystrokes", keystrokes_data)
     except Exception as e:
         print(e)
@@ -132,7 +146,7 @@ def submit():
 
     try:
         helpTaps_data = load_data_from_file("helpTaps")
-        helpTaps_data.append({"username": username, "helpTaps": helpTaps, "time": int(time.time())})
+        helpTaps_data.append({"username": username, "helpTaps": helpTaps, "time": int(time.time()), "currentPage": currentPage})
         save_data_to_file("helpTaps", helpTaps_data)
     except Exception as e:
         print(e)
